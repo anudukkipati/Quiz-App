c9f343b967580dc84a9ccc0c591d3bbc527a4f25
anupama <anupamagadde@yahoo.com>
2020-07-28 11:10:40 -0500

    add functions to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -15,23 +15,42 @@ let quiz = [
 //initialize a variable called score to keep track of the correct answers
 let score = 0;
 
-//loop through the quiz array to prompt question to users
+//invoke function play before the function as it will be hoisted
+play(quiz);
 
-for (let i = 0, max = quiz.length; i < max; i++) {
-    //prompt user the question
-   // let question = quiz[i][0];
-    //get answer from user
-    let answer = prompt(quiz[i][0]);
+//function definition
+function play(quiz) {
+    //main game loop
 
-    //check if answer is correct
-    if (answer === quiz[i][1]) {
-        alert("Correct");
-        //increase score for correct answer
-        score++;
-    } else {
-        alert("Wrong")
+    //declare variables outside for loop so that all functions will have access to them(let is not hoisted like var)
+    let i, question, answer, max;
+    for(i = 0; max = quiz.length, i < max; i++) {
+        question = quiz[i][0];
+        answer = ask(question);
+        check(answer);
+    }
+    //end of main game loop
+    gameOver();
+
+    //nested functions
+
+    function ask(question) {
+        return prompt(question);
+    }
+
+    function check(answer) {
+        //check if answer is correct
+        if (answer === quiz[i][1]) {
+            alert("Correct");
+            //increase score for correct answer
+            score++;
+        } else {
+            alert("Wrong")
+        }
+    }
+
+    function gameOver() {
+        alert("Game over, you scored " + score + " points");
     }
 }
 
-//tell user the score
-alert("Game over, you scored " + score + " points");
